41426d1327df55ad1c68deb0ef4ac080afffc97a
ZILLUR KHAN <jillurrahmancmt@gmail.com>
2023-02-04 02:59:03 +0600

    menu

---
+++ assets/js/menu.js
@@ -0,0 +1,42 @@
+(function ($) {
+    "use strict";
+    /*------------------------------------------------------------------
+    [Table of contents]
+
+    [1] navigation
+    [2] banner slider
+    [3] about video
+    [4] recent work tab
+    [5] funfact
+    [6] testmonial slider
+    [7] testmonial slider v2
+    [8] back to top
+    [9] wow animatation
+    [10] video popup
+    [11] our project grid layout
+    [12] gallery image popup
+    [13] header search
+    [14] about funfact
+    [15] company timeline
+    [16] testimonial slider
+    [17] contact form init
+    [18] preloader
+    -------------------------------------------------------------------*/
+
+
+
+
+    /*==========================================================
+      navigation
+     ======================================================================*/
+    if ($("#navigation1").length > 0) {
+        $("#navigation1").navigation({
+            effect: "slide"
+        });
+    }
+
+
+
+
+
+})(jQuery);
